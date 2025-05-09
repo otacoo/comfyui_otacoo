c919d3f5c7d2f26f661c55b711423245570a8c0a
otakudude <208893667+otacoo@users.noreply.github.com>
2025-05-09 22:22:11 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,9 @@
-NODE_CLASS_MAPPINGS = {}
+import os
 
+NODE_CLASS_MAPPINGS = {}
 WEB_DIRECTORY = "./js"
-__all__ = ["NODE_CLASS_MAPPINGS", "WEB_DIRECTORY"]
+__all__ = ["NODE_CLASS_MAPPINGS", "WEB_DIRECTORY", "setup"]
+
+def setup(app):
+    static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "web"))
+    app.router.add_static('/imginfo', static_path)
